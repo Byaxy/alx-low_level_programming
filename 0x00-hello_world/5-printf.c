74f6044487d7980c470f1b107aceb829f3679dfc
Byaxy <charlesbyaxy@gmail.com>
2021-06-17 10:30:59 -0700

    Print text with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - function to display text
+ *Return 0
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
