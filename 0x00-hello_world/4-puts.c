40cb8338ab506a7e2630e9fbf6cc0b1da60fa9d9
Byaxy <charlesbyaxy@gmail.com>
2021-06-17 10:19:28 -0700

    Print text with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - function to display text
+ *Return: 0
+ */
+
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
