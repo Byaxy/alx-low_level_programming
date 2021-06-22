bee0d7c5f35807e6ac739843f518d52821dd851f
Byaxy <charlesbyaxy@gmail.com>
2021-06-22 12:03:32 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *add - adds two numbers
+ *@x: first number
+ *@y: second number
+ *Return: the sum of the two numbers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
