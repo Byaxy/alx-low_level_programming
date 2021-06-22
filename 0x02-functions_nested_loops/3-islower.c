16ffb3776043c66af64b538e84803604eb3a829b
Byaxy <charlesbyaxy@gmail.com>
2021-06-22 08:14:40 -0700

    check if character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_islower - returns 1 if the character lowercase and 0 otherwise
+ *@c: The character to be checked
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
