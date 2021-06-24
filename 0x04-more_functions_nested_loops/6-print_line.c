b87f52b79751bd7c43639150dc10ddbb3995d516
Byaxy <charlesbyaxy@gmail.com>
2021-06-24 09:21:51 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws line on the terminal
+ * @n: lenth of line
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
