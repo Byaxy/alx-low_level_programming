e5774ae43a5177195d2ea4698240a3697b3ee471
Byaxy <charlesbyaxy@gmail.com>
2021-06-24 11:27:51 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
@@ -21,17 +25,13 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3) && (i % 5))
-		{
-			printf("FizzBuzz ");
-		}
 		else
 		{
 			printf("%d ", i);
 		}
 	}
 
-	_putchar('\n');
+	printf("\n");
 
 	return (0);
 }
