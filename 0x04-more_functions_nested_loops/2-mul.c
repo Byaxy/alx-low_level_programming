d4622dbd16c79c8df8e35aeec3d6e91ae552832b
Byaxy <charlesbyaxy@gmail.com>
2021-06-24 07:33:32 -0700

    Product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of the two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
