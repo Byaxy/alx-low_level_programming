d00279a1fe6649c5e9d6ed17bf820d74e08eed04
Byaxy <charlesbyaxy@gmail.com>
2021-06-24 07:49:39 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers 0 - 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
