57e3b57683698067ae5c0d5f89479e0960c8caa8
Byaxy <charlesbyaxy@gmail.com>
2021-06-28 10:43:35 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to reverse
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char *y = s;
+	int k = 0;
+	int x;
+	char n;
+
+	while (*y != '\0')
+	{
+		y++;
+		i++;
+	}
+
+	j  = i - 1;
+	for (; k < ((j / 2) + 1) ; j++)
+	{
+		x = (j - k);
+		n = s[k];
+		s[k] = s[x];
+		s[x] = n;
+	}
+}
