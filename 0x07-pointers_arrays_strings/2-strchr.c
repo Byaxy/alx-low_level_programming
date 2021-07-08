d12b39071879f8cf6cb7fdca63743d5500f298fb
Byaxy <charlesbyaxy@gmail.com>
2021-07-08 08:14:13 -0700

    find charater in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - find first occurance of a character in string
+ * @s: string
+ * @c: character to find
+ * Return: pointer to first occurance of character, NULL otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s != '\0' && *s != c)
+	{
+		i++;
+	}
+
+	if (*s == c)
+		return (&s[i]);
+	else
+		return (NULL);
+}
