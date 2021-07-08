b843836c72b96ec41473e0f54bc5d2d635c546c4
Byaxy <charlesbyaxy@gmail.com>
2021-07-08 09:14:35 -0700

    fix on 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*s != '\0' && *s != c)
+	while (s[i] != '\0' && s[i] != c)
 	{
 		i++;
 	}
 
-	if (*s == c)
+	if (s[i] == c)
 		return (&s[i]);
 	else
 		return (NULL);
