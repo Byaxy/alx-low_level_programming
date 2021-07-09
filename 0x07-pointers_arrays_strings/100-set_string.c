f33238104bf246b12b08b5d2ada6f21319006fd4
Byaxy <charlesbyaxy@gmail.com>
2021-07-09 00:33:45 -0700

    set pointer value to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - set value of string to char
+ * @s: string pointer
+ * @to: character pointer
+ * Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
