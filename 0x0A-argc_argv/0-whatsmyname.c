8f81150efd7fcc9bb08d0d194938c6092df0bc79
Byaxy <charlesbyaxy@gmail.com>
2021-07-12 04:29:18 -0700

    print programm name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints name of programm
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return(0);
+}
