0a05cf32881029ec10303c495d38b8cb45232330
Byaxy <charlesbyaxy@gmail.com>
2021-07-12 05:13:56 -0700

    print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,11 +8,11 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 	{
-		printf("%d\n", argc);
+		printf("%d\n", argc - 1);
 	}
 
 	return (0);
