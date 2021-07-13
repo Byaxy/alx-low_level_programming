f1119228807f60117f4f0adbcc816f37629219e0
Byaxy <charlesbyaxy@gmail.com>
2021-07-13 03:56:33 -0700

    concatenate all arguments to a program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: string lenght
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all arguments to a program
+ * @ac: argument counter
+ * @av: pointer to array of arguments
+ * Return: pointer concatenated arguments
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k = 0, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1);
+
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			str[k++] = av[i][j];
+		}
+		str[k++] = '\0';
+	}
+
+	return (str);
+}
