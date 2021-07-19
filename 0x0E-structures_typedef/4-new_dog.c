db9829b8bcd6eba628cfa9dd31717a4e18574027
Byaxy <charlesbyaxy@gmail.com>
2021-07-19 14:23:03 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,38 +3,6 @@
 #include "dog.h"
 
 /**
- * len - calculates the length of a string
- * @str: string
- * Return: length of the string
- */
-
-int len(char *str)
-{
-	int i;
-
-	for (i = 0; *(str + i); i++)
-		;
-	return (i);
-}
-
-
-/**
- * strcpy - copies src string to dest
- * @dest: destination of string
- * @src: sorce of string
- * Return: pointer to copied string
- */
-
-char *strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; i <= len(src); i++)
-		dest[i] = src[i];
-	return (dest);
-}
-
-/**
  * new_dog - creates a new instance of struct dog_t
  * @name: member
  * @age: member
@@ -44,6 +12,7 @@ char *strcpy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j, k;
 	dog_t *dog1;
 	char *new_name;
 	char *new_owner;
@@ -51,35 +20,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t));
 	if (dog1 == NULL)
 		return (NULL);
-
-	dog1->age = age;
-
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
 	if (name != NULL)
 	{
-		new_name = malloc(len(name) + 1);
+		new_name = malloc(sizeof(char) * i + 1);
 		if (new_name == NULL)
 		{
 			free(dog1);
 			return (NULL);
 		}
-		dog1->name = strcpy(new_name, name);
 	}
-	else
-		dog1->name = NULL;
-
 	if (owner != NULL)
 	{
-		new_owner = malloc(len(owner) + 1);
+		new_owner = malloc(sizeof(char) * j + 1);
 		if (new_owner == NULL)
 		{
 			free(new_name);
 			free(dog1);
 			return (NULL);
 		}
-		dog1->owner = strcpy(new_owner, owner);
 	}
-	else
-		dog1->owner = NULL;
+	for (k = 0; k <= i; k++)
+		new_name[k] = name[k];
+	for (k = 0; k <= j; k++)
+		new_owner[k] = owner[k];
+	dog1->name = new_name;
+	dog1->age = age;
+	dog1->owner = new_owner;
 
 	return (dog1);
 }
