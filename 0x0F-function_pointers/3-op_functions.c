7553f9487c337bcf322def5c6580de128d978f19
Byaxy <charlesbyaxy@gmail.com>
2021-07-22 07:26:50 -0700

    simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
