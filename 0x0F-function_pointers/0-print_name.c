ce25775762d4f7fdcf04ecd8220a940fb824a242
Byaxy <charlesbyaxy@gmail.com>
2021-07-22 05:00:23 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - calls a function passed as a parameter
+ * @name: parameter for function call
+ * @f: pointer to function to be called
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
