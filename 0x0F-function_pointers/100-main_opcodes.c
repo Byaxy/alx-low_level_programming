1309137e384582ca4ab0513860b46167d27329d6
Byaxy <charlesbyaxy@gmail.com>
2021-07-22 08:05:13 -0700

    print op codes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * main - print opcodes of its own main function
+ * @argc: arguments counter
+ * @argv: arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, num;
+	char *p = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num - 1; i++)
+	{
+		printf("%02hhx ", p[i]);
+	}
+	printf("%02hhx\n", p[i]);
+
+	return (0);
+}
