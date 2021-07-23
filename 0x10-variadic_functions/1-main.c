f6f875954c3c70513c21db60c1da52bf90e5b227
Byaxy <charlesbyaxy@gmail.com>
2021-07-23 04:46:41 -0700

    fixed all

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check 1-print_numbers.c code
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+	return (0);
+}
---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check 3-print_all.c code
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	return (0);
+}
