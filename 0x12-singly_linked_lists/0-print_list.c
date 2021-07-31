38a24ba070eb72e1f154a0c75ada4cffa81a557f
Byaxy <charlesbyaxy@gmail.com>
2021-07-31 05:25:11 -0700

    print linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all members of a linked list
+ * @h: pointer to the linked list
+ * Return: number of nodes in the linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
