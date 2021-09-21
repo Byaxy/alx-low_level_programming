53a1f5f6824584b9fa1e8e4b725ad95703f14b0d
Byaxy <charlesbyaxy@gmail.com>
2021-09-21 05:12:18 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/C-files/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: destination of copied string
+ * @src: string to be copied
+ * @n: number of bytes of string to copy
+ * Return: copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/C-files/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *_puts - prints a string
+ * @str: pointer to the string
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
